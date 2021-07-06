7e11ae0ff1043110e319b712527a825b2a211a5e
Your <handle@email.com>
2021-07-06 01:54:25 -0700

    add ground and character

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,12 +5,17 @@ class Play extends Phaser.Scene {
     preload() {
         // load images/tile sprites
         this.load.image('space', './assets/space.png');
-      }
+        this.load.image('ground', './assets/Ground.png');
+        this.load.image('rabbit','./assets/rabbit.png')
+    }
     create(){
         this.add.text(20, 20, "Endless Runner Play");
         this.space = this.add.tileSprite(0, 0, 640, 480, 'space').setOrigin(0, 0);
+        this.ground = this.add.tileSprite(0, game.config.height-60, game.config.width, 60 , 'ground').setOrigin(0, 0);
+        this.rabbit = this.add.sprite(32, game.config.height-110, 'rabbit').setOrigin(0.5);
     }
     update() {
-        this.space.tilePositionX -= 4;
+        this.space.tilePositionX += 4;
+        this.ground.tilePositionX += 4;
       }
 }
